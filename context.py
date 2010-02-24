a253d690b9575018d8318b54cadc5c6455e090b1
Andrew Kelley <superjoe30@gmail.com>
2010-02-23 19:41:15 -0700

    home page mostly works

--- context.py
+++ context.py
@@ -6,5 +6,6 @@ import datetime
 def global_values(request):
     recent_time = datetime.datetime.today() - datetime.timedelta(minutes=10)
     online_profiles = Profile.objects.filter(date_activity__gte=recent_time)
+    user = request.user
 
     return locals();
