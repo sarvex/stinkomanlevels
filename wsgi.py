f0ad67633b0486d32447bf4121a04160db763a13
Andrew Kelley <superjoe30@gmail.com>
2012-12-08 11:56:25 -0500

    more things in wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,10 @@ import os
 import sys
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 new_path = os.path.dirname(__file__)
+new_path2 = os.path.join(new_path, "..")
 if new_path not in sys.path:
     sys.path.append(new_path)
+if new_path2 not in sys.path:
+    sys.path.append(new_path2)
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
